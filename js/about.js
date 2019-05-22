b17d1ecca8b2676376948165f2354af6f049ff56
Megan <megan.miller@smoldev.dev>
2019-05-22 12:50:31 -0500

    Assembly required
    Added:
    - Marketing Text
    - Marketing Images
    - aboutUs Object
    - images of team members
    - About text for:
       - Sameera

---
+++ js/about.js
@@ -0,0 +1,42 @@
+const aboutUs = {
+    "TeamLead": {
+        "name": "Justin Lowry",
+        "image": "./img/Justin-Lowry.png",
+        "about": "a couple sentences here",
+    },
+    "FrontEndArchitect": {
+        "name": "Josh Timmons",
+        "image": "./img/Josh-Timmons.png",
+        "about": "a couple sentences here",
+    },
+    "BackEndArchitect": {
+        "name": "Tyler Foreman",
+        "image": "./img/Tyler-Foreman.png",
+        "about": "a couple sentences here",
+    },
+    "UXDesigner": {
+        "name": "Justin Stevens",
+        "image": "./img/Justin-Stevens.png",
+        "about": "a couple sentences here",
+    },
+    "IOSDeveloper": {
+        "name": "Sameera Leola",
+        "image": "./img/Sameera-Leola.png",
+        "about": "In iOS6, live in Chicago and enjoying our beautiful fall weather in May, Love reading: Sci-Fi and Fantasy new and old.  Like audiobooks more than movies.  I've read/listened to the Harry Potter series more times than I can count.",
+    },
+    "UIDev1": {
+        "name": "Blair Reynolds",
+        "image": "./img/Blair-Reynolds.png",
+        "about": "a couple sentences here",
+    },
+    "UIDev2": {
+        "name": "Kevin Sims",
+        "image": "./img/Kevin-Sims.png",
+        "about": "a couple sentences here",
+    },
+    "UIDev3": {
+        "name": "Megan Miller",
+        "image": "./img/Me.png",
+        "about": "a couple sentences here"
+    }
+}
